5beda3068d0d6e7c60ea103b93353f824ac9607d
Vlad <lubinvlad@icloud.com>
2022-06-08 22:58:14 +0300

    splash screen

--- app/src/main/java/crazyson/com/ua/crypton/MainActivity.kt
+++ app/src/main/java/crazyson/com/ua/crypton/MainActivity.kt
@@ -3,11 +3,14 @@ package crazyson.com.ua.crypton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import crazyson.com.ua.crypton.presentation.MainViewModel
 import crazyson.com.ua.crypton.presentation.Screen
 import crazyson.com.ua.crypton.presentation.coin_detail.CoinDetailScreen
 import crazyson.com.ua.crypton.presentation.coin_list.CoinListScreen
@@ -16,8 +19,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
+        }
+
         setContent {
             CryptonTheme {
                 Surface(color = MaterialTheme.colors.background) {
