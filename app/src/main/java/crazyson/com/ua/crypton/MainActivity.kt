129197de84d878c783215efb7516581e333afe48
Vlad <lubinvlad2@gmail.com>
2025-10-30 01:42:29 +0200

    edge 2 edge

--- app/src/main/java/crazyson/com/ua/crypton/MainActivity.kt
+++ app/src/main/java/crazyson/com/ua/crypton/MainActivity.kt
@@ -3,6 +3,7 @@ package crazyson.com.ua.crypton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -22,6 +23,8 @@ class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
 
         installSplashScreen().apply {
             setKeepOnScreenCondition {
